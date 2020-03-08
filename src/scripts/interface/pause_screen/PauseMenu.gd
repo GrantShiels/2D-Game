dd81236b6145a34bb93b2dadfb2898a567868922
Grant Mac <grant.shiels@btinternet.com>
2020-03-08 17:52:03 +0000

    Added UI to test level
    Implemented the UI into the test level and added pause functionality.
    Exit level and such works.

--- src/scripts/interface/pause_screen/PauseMenu.gd
+++ src/scripts/interface/pause_screen/PauseMenu.gd
@@ -1,31 +1,36 @@
 #All script for the in game UI
 extends Control
 
+#When the scene first loads get the current scene tree and get the pause menu
+onready var scene_tree: = get_tree()
+onready var pause_overlay: ColorRect = get_node("Pause Overlay")
+
+var paused: = false setget set_paused
+
+#If user presses pause key (Esc) then invert the pause value.
+func _unhandled_input(event: InputEvent) -> void: 
+	if event.is_action_pressed("pause"):
+		self.paused = not paused
+		scene_tree.set_input_as_handled()
+	
+
+func set_paused(value:bool) -> void:
+	paused = value
+	scene_tree.paused = value
+	pause_overlay.visible = value
+	
 
 
 
 
 
 
-
-
-
-
-
-#-----------------------
-#PAUSE MENU CODE
-#-----------------------
-#All code for the pause menu is within this chunk
 
 #When the quit game button is pressed the user will be taken to the title screen.
 func _on_Quit_Game_Button_pressed():
 	get_tree().change_scene("res://src/interface/title_screen/TitleScreen.tscn")
+	paused = false
 
 #When user presses button the game will no longer be paused.
 func _on_Resume_Game_Button_pressed():
 	get_tree().paused = false
-
-
-#-----------------------
-#PAUSE MENU CODE
-#-----------------------
