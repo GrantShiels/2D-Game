67ca8e5825e75572a4cb3068ab3ddd8a1f649c3a
Grant Mac <grant.shiels@btinternet.com>
2020-03-08 17:20:33 +0000

    Minor readability changes to code

--- src/scripts/interface/pause_screen/PauseMenu.gd
+++ src/scripts/interface/pause_screen/PauseMenu.gd
@@ -1,10 +1,31 @@
+#All script for the in game UI
 extends Control
+
+
+
+
+
+
+
+
+
+
+
+
+#-----------------------
+#PAUSE MENU CODE
+#-----------------------
+#All code for the pause menu is within this chunk
 
 #When the quit game button is pressed the user will be taken to the title screen.
 func _on_Quit_Game_Button_pressed():
 	get_tree().change_scene("res://src/interface/title_screen/TitleScreen.tscn")
 
-
 #When user presses button the game will no longer be paused.
 func _on_Resume_Game_Button_pressed():
 	get_tree().paused = false
+
+
+#-----------------------
+#PAUSE MENU CODE
+#-----------------------
