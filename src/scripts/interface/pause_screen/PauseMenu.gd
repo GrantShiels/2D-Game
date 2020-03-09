e98acc206f74154bfcee3ce97e8ae3f6786409b4
Grant Mac <grant.shiels@btinternet.com>
2020-03-09 14:50:03 +0000

    Fixed Pause Issue
    Made change to quit level button so that the whole system isn't still paused when the user quits back to the menu

--- src/scripts/interface/pause_screen/PauseMenu.gd
+++ src/scripts/interface/pause_screen/PauseMenu.gd
@@ -29,7 +29,7 @@ func set_paused(value:bool) -> void:
 #When the quit game button is pressed the user will be taken to the title screen.
 func _on_Quit_Game_Button_pressed():
 	get_tree().change_scene("res://src/interface/title_screen/TitleScreen.tscn")
-	paused = false
+	self.paused = not paused
 
 #When user presses button the game will no longer be paused.
 func _on_Resume_Game_Button_pressed():
