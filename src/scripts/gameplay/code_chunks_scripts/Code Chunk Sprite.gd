aa6eef3ae98980da35a58202cee3627dedc468a4
Grant Mac <grant.shiels@btinternet.com>
2020-04-11 16:12:40 +0100

    Removed old function

--- src/scripts/gameplay/code_chunks_scripts/Code Chunk Sprite.gd
+++ src/scripts/gameplay/code_chunks_scripts/Code Chunk Sprite.gd
@@ -8,23 +8,6 @@ var mouse_in = false
 #checks to see if the sprite is being dragged
 var dragging = false
 
-#func _input(event):
-#	#If left mouse button is pressed then
-#	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
-#		#If the mouse is within the sprite
-#		if mouse_in == true:
-#			#If the button is pressed and draggin is false
-#			if event.pressed and !dragging:
-#				dragging = true
-#
-#		#If the button is being pressed and button is let go
-#		if !event.pressed and dragging:
-#			dragging = false
-#
-#	if event is InputEventMouseMotion and dragging:
-#		#While dragging is true and button pressed move the area with the mouse
-#		$Area2D.position = event.position
-
 
 func _process(delta):
 	#if mouse in and mouse button pressed
